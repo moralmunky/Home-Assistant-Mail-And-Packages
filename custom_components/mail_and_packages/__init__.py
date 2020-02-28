18c1c36c304cd32536229bae3d4c46c387a147ba
Chris <firstof9@gmail.com>
2020-02-28 13:02:48 -0700

    Formatting

--- custom_components/mail_and_packages/__init__.py
+++ custom_components/mail_and_packages/__init__.py
@@ -27,9 +27,6 @@ async def async_setup_entry(hass, config_entry):
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
     )
-    # hass.async_create_task(
-    #     hass.config_entries.async_forward_entry_setup(entry, "camera")
-    # )
 
     return True
 
