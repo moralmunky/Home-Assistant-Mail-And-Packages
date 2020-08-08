a8abf7ae15f2663563920c40091fbf1355ad3aa6
firstof9 <firstof9@gmail.com>
2020-08-08 15:04:30 -0700

    Fix indeting

--- custom_components/mail_and_packages/sensor.py
+++ custom_components/mail_and_packages/sensor.py
@@ -653,7 +653,8 @@ def get_count(account, sensor_type, get_tracking_num=False, image_path=None, has
         # Try to guard against duplicate emails via tracking number
         if len(tracking) < len(count):
             count = len(tracking)
-        result[ATTR_TRACKING] = tracking
+    
+    result[ATTR_TRACKING] = tracking
 
     result[ATTR_COUNT] = count
     return result
