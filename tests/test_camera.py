64302f9174b119905de45ca1560dea5ba62fd16f
firstof9 <firstof9@gmail.com>
2021-03-19 15:52:45 -0700

    More tests

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -1,10 +1,10 @@
 """Tests for camera component."""
 import logging
-from unittest.mock import patch
+from unittest.mock import call, mock_open, patch
 
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
-from custom_components.mail_and_packages.const import DOMAIN
+from custom_components.mail_and_packages.const import DOMAIN, CAMERA
 from tests.const import FAKE_CONFIG_DATA
 
 _LOGGER = logging.getLogger(__name__)
@@ -89,3 +89,112 @@ async def test_check_file_path_access(
         assert await hass.config_entries.async_setup(entry.entry_id)
         await hass.async_block_till_done()
         assert "Could not read camera" in caplog.text
+
+
+async def test_async_camera_image(
+    hass,
+    mock_imap_no_email,
+    mock_osremove,
+    mock_osmakedir,
+    mock_listdir,
+    mock_update_time,
+    mock_copy_overlays,
+    mock_hash_file,
+    mock_getctime_today,
+):
+    """Test async_camera_image function."""
+
+    with patch("os.path.isfile", return_value=True), patch(
+        "os.access", return_value=False
+    ):
+        entry = MockConfigEntry(
+            domain=DOMAIN,
+            title="imap.test.email",
+            data=FAKE_CONFIG_DATA,
+        )
+
+        entry.add_to_hass(hass)
+        assert await hass.config_entries.async_setup(entry.entry_id)
+        await hass.async_block_till_done()
+
+        cameras = hass.data[DOMAIN][entry.entry_id][CAMERA]
+        m_open = mock_open()
+        with patch("builtins.open", m_open, create=True):
+            image = await cameras[0].async_camera_image()
+
+        assert m_open.call_count == 1
+        assert (
+            "custom_components/mail_and_packages/mail_none.gif"
+            in m_open.call_args.args[0]
+        )
+        assert m_open.call_args.args[1] == "rb"
+
+
+async def test_async_camera_image_file_error(
+    hass,
+    mock_imap_no_email,
+    mock_osremove,
+    mock_osmakedir,
+    mock_listdir,
+    mock_update_time,
+    mock_copy_overlays,
+    mock_hash_file,
+    mock_getctime_today,
+    caplog,
+):
+    """Test async_camera_image function."""
+
+    with patch("os.path.isfile", return_value=True), patch(
+        "os.access", return_value=False
+    ):
+        entry = MockConfigEntry(
+            domain=DOMAIN,
+            title="imap.test.email",
+            data=FAKE_CONFIG_DATA,
+        )
+
+        entry.add_to_hass(hass)
+        assert await hass.config_entries.async_setup(entry.entry_id)
+        await hass.async_block_till_done()
+
+        cameras = hass.data[DOMAIN][entry.entry_id][CAMERA]
+        m_open = mock_open()
+        with patch("builtins.open", m_open, create=True):
+            m_open.side_effect = FileNotFoundError
+            image = await cameras[0].async_camera_image()
+
+        assert "Could not read camera" in caplog.text
+
+
+async def test_async_on_demand_update(
+    hass,
+    mock_imap_no_email,
+    mock_osremove,
+    mock_osmakedir,
+    mock_listdir,
+    mock_update_time,
+    mock_copy_overlays,
+    mock_hash_file,
+    mock_getctime_today,
+):
+    """Test async_camera_image function."""
+
+    with patch("os.path.isfile", return_value=True), patch(
+        "os.access", return_value=False
+    ):
+        entry = MockConfigEntry(
+            domain=DOMAIN,
+            title="imap.test.email",
+            data=FAKE_CONFIG_DATA,
+        )
+
+        entry.add_to_hass(hass)
+        assert await hass.config_entries.async_setup(entry.entry_id)
+        await hass.async_block_till_done()
+
+        cameras = hass.data[DOMAIN][entry.entry_id][CAMERA]
+        m_open = mock_open()
+        with patch("builtins.open", m_open, create=True):
+            image = await cameras[0].async_on_demand_update()
+
+        assert image is None
