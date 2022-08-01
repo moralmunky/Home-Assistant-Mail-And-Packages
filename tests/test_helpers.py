a0be002aad5bcdb15684ee8c7e1a3fe90e70cb2a
Chris <firstof9@gmail.com>
2022-08-01 10:01:31 -0700

    update tests

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -796,7 +796,7 @@ async def test_amazon_search_results(hass, mock_imap_amazon_shipped):
     result = amazon_search(
         mock_imap_amazon_shipped, "test/path", hass, "testfilename.jpg"
     )
-    assert result == 40
+    assert result == 44
 
 
 async def test_amazon_search_delivered(
@@ -805,7 +805,7 @@ async def test_amazon_search_delivered(
     result = amazon_search(
         mock_imap_amazon_delivered, "test/path", hass, "testfilename.jpg"
     )
-    assert result == 40
+    assert result == 44
     assert mock_download_img.called
 
 
@@ -815,7 +815,7 @@ async def test_amazon_search_delivered_it(
     result = amazon_search(
         mock_imap_amazon_delivered_it, "test/path", hass, "testfilename.jpg"
     )
-    assert result == 40
+    assert result == 44
 
 
 async def test_amazon_hub(hass, mock_imap_amazon_the_hub):
@@ -1026,7 +1026,7 @@ async def test_image_file_name(
 
 async def test_amazon_exception(hass, mock_imap_amazon_exception, caplog):
     result = amazon_exception(mock_imap_amazon_exception, ['""'])
-    assert result["order"] == ["123-1234567-1234567"] * 10
+    assert result["order"] == ["123-1234567-1234567"] * 11
     assert result["count"] == 10
 
     result = amazon_exception(mock_imap_amazon_exception, ["testemail@fakedomain.com"])
