e38103d8f6dc54799ecf81282c3c87c9b9777804
Alex L <9060360+AlexLamond@users.noreply.github.com>
2022-11-28 16:49:12 +0000

    Change version back

--- custom_components/mail_and_packages/const.py
+++ custom_components/mail_and_packages/const.py
@@ -12,7 +12,7 @@ from homeassistant.helpers.entity import EntityCategory
 
 DOMAIN = "mail_and_packages"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "3-dev"  # Now updated by release workflow
+VERSION = "0.0.0-dev"  # Now updated by release workflow
 ISSUE_URL = "http://github.com/moralmunky/Home-Assistant-Mail-And-Packages"
 PLATFORM = "sensor"
 PLATFORMS = ["binary_sensor", "camera", "sensor"]
