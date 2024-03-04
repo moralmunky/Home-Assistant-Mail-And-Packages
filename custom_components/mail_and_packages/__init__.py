b3dd8338d8d5f75209dee5fa3d5cabf8d3c837fd
firstof9@gmail.com <firstof9@gmail.com>
2024-03-04 14:52:16 -0700

    refactor: maintenace update for 2024.3.x+

--- custom_components/mail_and_packages/__init__.py
+++ custom_components/mail_and_packages/__init__.py
@@ -84,7 +84,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
 
     config_entry.add_update_listener(update_listener)
 
-    config_entry.options = config_entry.data
+    await hass.config_entries.async_update_entry(options=config_entry.data)
     config = config_entry.data
 
     # Variables for data coordinator
