fcd0fbf22e7b642451d7c1ad7dcbd2510502ae9d
firstof9@gmail.com <firstof9@gmail.com>
2024-03-07 13:27:06 -0700

    fix: adjust configuration validation in config flow

--- custom_components/mail_and_packages/config_flow.py
+++ custom_components/mail_and_packages/config_flow.py
@@ -159,10 +159,10 @@ def _get_schema_step_1(user_input: list, default_dict: list) -> Any:
 
     return vol.Schema(
         {
-            vol.Required(CONF_HOST, default=_get_default(CONF_HOST)): str,
-            vol.Required(CONF_PORT, default=_get_default(CONF_PORT)): vol.Coerce(int),
-            vol.Required(CONF_USERNAME, default=_get_default(CONF_USERNAME)): str,
-            vol.Required(CONF_PASSWORD, default=_get_default(CONF_PASSWORD)): str,
+            vol.Required(CONF_HOST, default=_get_default(CONF_HOST)): cv.string,
+            vol.Required(CONF_PORT, default=_get_default(CONF_PORT, 993)): cv.port,
+            vol.Required(CONF_USERNAME, default=_get_default(CONF_USERNAME)): cv.string,
+            vol.Required(CONF_PASSWORD, default=_get_default(CONF_PASSWORD)): cv.string,
         }
     )
 
@@ -189,24 +189,24 @@ def _get_schema_step_2(data: list, user_input: list, default_dict: list) -> Any:
             vol.Required(
                 CONF_RESOURCES, default=_get_default(CONF_RESOURCES)
             ): cv.multi_select(get_resources()),
-            vol.Optional(CONF_AMAZON_FWDS, default=_get_default(CONF_AMAZON_FWDS)): str,
+            vol.Optional(CONF_AMAZON_FWDS, default=_get_default(CONF_AMAZON_FWDS,"")): cv.string,
             vol.Optional(CONF_AMAZON_DAYS, default=_get_default(CONF_AMAZON_DAYS)): int,
             vol.Optional(
-                CONF_SCAN_INTERVAL, default=_get_default(CONF_SCAN_INTERVAL)
+                CONF_SCAN_INTERVAL, default=_get_default(CONF_SCAN_INTERVAL, 30)
             ): vol.All(vol.Coerce(int)),
             vol.Optional(
-                CONF_IMAP_TIMEOUT, default=_get_default(CONF_IMAP_TIMEOUT)
+                CONF_IMAP_TIMEOUT, default=_get_default(CONF_IMAP_TIMEOUT, 30)
             ): vol.All(vol.Coerce(int)),
             vol.Optional(
                 CONF_DURATION, default=_get_default(CONF_DURATION)
             ): vol.Coerce(int),
             vol.Optional(
                 CONF_GENERATE_MP4, default=_get_default(CONF_GENERATE_MP4)
-            ): bool,
+            ): cv.boolean,
             vol.Optional(
                 CONF_ALLOW_EXTERNAL, default=_get_default(CONF_ALLOW_EXTERNAL)
-            ): bool,
-            vol.Optional(CONF_CUSTOM_IMG, default=_get_default(CONF_CUSTOM_IMG)): bool,
+            ): cv.boolean,
+            vol.Optional(CONF_CUSTOM_IMG, default=_get_default(CONF_CUSTOM_IMG)): cv.boolean,
         }
     )
 
@@ -225,7 +225,7 @@ def _get_schema_step_3(user_input: list, default_dict: list) -> Any:
             vol.Optional(
                 CONF_CUSTOM_IMG_FILE,
                 default=_get_default(CONF_CUSTOM_IMG_FILE, DEFAULT_CUSTOM_IMG_FILE),
-            ): str,
+            ): cv.string,
         }
     )
 
