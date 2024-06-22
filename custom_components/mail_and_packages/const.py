7d52bceb1286be42215ab0a489d01a5d25a71ae7
Chris <firstof9@gmail.com>
2024-06-22 16:09:46 -0700

    fix: usps_mail_delivered device_class (#923)
    * fix: usps_mail_delivered device_class
    
    * disable sensor by default
    
    * fix test

--- custom_components/mail_and_packages/const.py
+++ custom_components/mail_and_packages/const.py
@@ -1133,7 +1133,7 @@ BINARY_SENSORS: Final[dict[str, BinarySensorEntityDescription]] = {
     "usps_mail_delivered": BinarySensorEntityDescription(
         name="USPS Mail Delivered",
         key="usps_mail_delivered",
-        device_class=BinarySensorDeviceClass.UPDATE,
+        entity_registry_enabled_default=False,
     ),
 }
 
