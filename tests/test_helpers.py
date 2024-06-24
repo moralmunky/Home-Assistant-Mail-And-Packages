8a4475bb5140d15f364a5cfc0ca95f8a07873f9e
firstof9@gmail.com <firstof9@gmail.com>
2024-06-23 17:11:39 -0700

    update tests

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -781,7 +781,7 @@ async def test_amazon_search_results(hass, mock_imap_amazon_shipped):
     result = amazon_search(
         mock_imap_amazon_shipped, "test/path", hass, "testfilename.jpg"
     )
-    assert result == 78
+    assert result == 98
 
 
 @pytest.mark.asyncio
@@ -791,7 +791,7 @@ async def test_amazon_search_delivered(
     result = amazon_search(
         mock_imap_amazon_delivered, "test/path", hass, "testfilename.jpg"
     )
-    assert result == 78
+    assert result == 98
     assert mock_download_img.called
 
 
@@ -802,7 +802,7 @@ async def test_amazon_search_delivered_it(
     result = amazon_search(
         mock_imap_amazon_delivered_it, "test/path", hass, "testfilename.jpg"
     )
-    assert result == 78
+    assert result == 98
 
 
 @pytest.mark.asyncio
@@ -1035,13 +1035,13 @@ async def test_image_file_name(
 @pytest.mark.asyncio
 async def test_amazon_exception(hass, mock_imap_amazon_exception, caplog):
     result = amazon_exception(mock_imap_amazon_exception, ['""'])
-    assert result["order"] == ["123-1234567-1234567"] * 13
-    assert result["count"] == 13
+    assert result["order"] == ["123-1234567-1234567"] * 14
+    assert result["count"] == 14
 
     result = amazon_exception(mock_imap_amazon_exception, ["testemail@fakedomain.com"])
-    assert result["count"] == 14
+    assert result["count"] == 15
     assert (
-        "Amazon domains to be checked: ['amazon.com', 'amazon.ca', 'amazon.co.uk', 'amazon.in', 'amazon.de', 'amazon.it', 'amazon.com.au', 'amazon.pl', 'amazon.es', 'amazon.fr', 'amazon.ae', 'fakeuser@fake.email', 'fakeuser2@fake.email', 'testemail@fakedomain.com']"
+        "Amazon domains to be checked: ['amazon.com', 'amazon.ca', 'amazon.co.uk', 'amazon.in', 'amazon.de', 'amazon.it', 'amazon.com.au', 'amazon.pl', 'amazon.es', 'amazon.fr', 'amazon.ae', 'amazon.nl', 'fakeuser@fake.email', 'fakeuser2@fake.email', 'testemail@fakedomain.com']"
         in caplog.text
     )
 
