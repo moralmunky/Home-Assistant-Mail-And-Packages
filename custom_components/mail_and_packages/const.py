6b9229cdf1c3e693199b7a479cf33ab2ae25a269
ChrisLanks <ChrisLanks27@gmail.com>
2025-08-07 02:13:27 -0400

    Update custom_components/mail_and_packages/const.py
    Co-authored-by: Chris <1105672+firstof9@users.noreply.github.com>

--- custom_components/mail_and_packages/const.py
+++ custom_components/mail_and_packages/const.py
@@ -12,7 +12,7 @@ from .entity import MailandPackagesBinarySensorEntityDescription
 
 DOMAIN = "mail_and_packages"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.4.3-b15"
+VERSION = "0.0.0-dev"  # Now updated by release workflow
 ISSUE_URL = "http://github.com/moralmunky/Home-Assistant-Mail-And-Packages"
 PLATFORM = "sensor"
 PLATFORMS = ["binary_sensor", "camera", "sensor"]
