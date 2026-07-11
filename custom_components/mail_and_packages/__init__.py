b08c3a4166baf3578991ba8f6ced96b1a4e59160
firstof9@gmail.com <firstof9@gmail.com>
2026-07-11 16:58:24 -0700

    fix(translation): resolve strings.json validation error and CONFIG_SCHEMA warning

--- custom_components/mail_and_packages/__init__.py
+++ custom_components/mail_and_packages/__init__.py
@@ -7,8 +7,15 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_RESOURCES
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryAuthFailed, ConfigEntryNotReady
-from homeassistant.helpers import device_registry as dr
-from homeassistant.helpers import issue_registry as ir
+from homeassistant.helpers import (
+    config_validation as cv,
+)
+from homeassistant.helpers import (
+    device_registry as dr,
+)
+from homeassistant.helpers import (
+    issue_registry as ir,
+)
 
 from . import const
 from .const import (
@@ -102,6 +109,9 @@ __all__ = [
 ]
 
 _LOGGER = logging.getLogger(__name__)
+
+
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
 
 async def async_setup(hass: HomeAssistant, config_entry: MailAndPackagesConfigEntry):  # pylint: disable=unused-argument
