8dc74c7741db521de0c1a228a2fa0f3a4d240e31
firstof9@gmail.com <firstof9@gmail.com>
2026-07-25 09:25:37 -0700

    fix(sensor): retain last state on coordinator update failure

--- custom_components/mail_and_packages/camera.py
+++ custom_components/mail_and_packages/camera.py
@@ -659,11 +659,6 @@ class MailCam(CoordinatorEntity, Camera):
         """
         return False
 
-    @property
-    def available(self) -> bool:
-        """Return if entity is available."""
-        return self.coordinator.data is not None
-
     def _handle_coordinator_update(self) -> None:
         """Handle updated data from the coordinator."""
         _LOGGER.debug(
