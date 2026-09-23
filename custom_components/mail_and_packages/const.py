2352c94458cb103bc7e7d3b73dc31ffc620c1fd6
Chris <1105672+firstof9@users.noreply.github.com>
2026-09-23 08:37:34 -0700

    fix(amazon): support itemized shipped and ordered email subjects (fixes #1465) (#1466)

--- custom_components/mail_and_packages/const.py
+++ custom_components/mail_and_packages/const.py
@@ -196,7 +196,7 @@ AMAZON_DELIVERING_SUBJECT = [
     "En cours de livraison",
 ]
 AMAZON_SHIPMENT_SUBJECT = [
-    "Shipped:",
+    "Shipped",
     "Enviado:",
     "Spedito:",
     "Versandt:",
@@ -205,7 +205,7 @@ AMAZON_SHIPMENT_SUBJECT = [
     *AMAZON_DELIVERING_SUBJECT,
 ]
 AMAZON_ORDERED_SUBJECT = [
-    "Ordered:",
+    "Ordered",
     "Pedido efetuado:",
     "Commandé",
 ]
